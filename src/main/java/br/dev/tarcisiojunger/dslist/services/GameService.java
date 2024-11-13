5e902816a36ad189a24d9f878606c632b27799d8
Tarcisio Paz Junger <tarcisiojunger@gmail.com>
2024-11-13 00:44:14 -0300

    GET game by id

--- src/main/java/br/dev/tarcisiojunger/dslist/services/GameService.java
+++ src/main/java/br/dev/tarcisiojunger/dslist/services/GameService.java
@@ -4,7 +4,9 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import br.dev.tarcisiojunger.dslist.dto.GameDTO;
 import br.dev.tarcisiojunger.dslist.dto.GameMinDTO;
 import br.dev.tarcisiojunger.dslist.entities.Game;
 import br.dev.tarcisiojunger.dslist.repositories.GameRepository;
@@ -15,10 +17,18 @@ public class GameService {
 	@Autowired
 	private GameRepository gameRepository;
 	
+	@Transactional(readOnly = true)
+	public GameDTO findById(Long gameId) {
+		Game result = gameRepository.findById(gameId).get();
+		GameDTO dto = new GameDTO(result);
+		return dto;
+	}
+	
+	@Transactional(readOnly = true)
 	public List<GameMinDTO> findAll(){
 		List<Game> result = gameRepository.findAll();
-		return result.stream().map(x -> new GameMinDTO(x)).toList();
-		
-		
+		return result.stream().map(x -> new GameMinDTO(x)).toList();	
 	}
+	
+	
 }
